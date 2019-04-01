aeab843b9efa615c926f6fd8c39bf26af76daa29
Bryan Costa <bryananthony.costa@gmail.com>
2019-04-01 17:35:16 -0400

    modularized code with inquirer prompts

--- bin/fullConfig.js
+++ bin/fullConfig.js
@@ -1,99 +1,118 @@
-'use strict';
-const webpack = require ('./action_webpack'); 
-const createServer = require ('./action_server'); 
-const react = require('./action_react')
-const pg = require('./action_pg')
-const mongo = require('./action_mongo')
+"use strict";
 
-module.exports.fullConfig = function () {
-  var inquirer = require('inquirer');
-  const clear = require('clear');
-  clear();
+const inquirer = require("inquirer");
+const clear = require("clear");
+const webpack = require("./action_webpack");
+const createServer = require("./action_server");
+const react = require("./action_react");
+const pg = require("./action_pg");
+const mongo = require("./action_mongo");
 
-  const setupServer = {
-      PORT: 3000,  
-      bodyParser: false, 
-      cors: false, 
-      mongoose: {
-      }
-    }
+module.exports.fullConfig = function() {
+  // const setupServer = {
+  //   PORT: 3000,
+  //   bodyParser: false,
+  //   cors: false,
+  //   mongoose: {}
+  // };
 
-    const serverQuestions = [
-      { // PORT 
-        type: 'input',
-        name: 'PORT',
-        message: 'PORT (default:3000) ',
-        default: 3000,
-        validate: (value)=>{
-          if( Number.isInteger(parseInt(value)) && value > 0 ){
-            return true;
-          } else {
-            return 'please enter a valid number';
-          }
+  const serverQuestions = [
+    {
+      // PORT
+      type: "input",
+      name: "PORT",
+      message: "PORT (default:3000) ",
+      default: 3000,
+      validate: value => {
+        if (Number.isInteger(parseInt(value)) && value > 0) {
+          return true;
         }
-      },
-      { // App Middleware
-        type: 'checkbox',
-        name: 'AppMiddleware',
-        mesasge: 'Would you like to include the following App Middleware ?',
-        choices: [
-          {name: 'Body Parser', value:'bodyParser',checked: true},
-          {name: 'cors', value:`cors`,checked: true}
-        ]
-      //, default: ['bodyParser','cors']
-      },
-      { // mongoose ? yes/no 
-        type: 'list',
-        name: 'mongoose',
-        message: 'Would you like to Include Mongoose ?',
-        choices:[
-          {name: 'Yes', value:true},
-          {name: 'No', value:false}
-        ]
-      },
-      { // further setup mongoose ? 
-        type: 'list',
-        name: 'setupMongoose',
-        message: 'Do you want to configure/setup Mongoose ?',
-        choices:[
-          {name: 'Yes', value:true},
-          {name: 'No', value:false}
-        ],
-        when: (answers => answers.mongoose ) // only ask if User includes Mongoose 
-      },
-      { // Mongoose collection name
-        type: 'input',
-        name: 'collectionName',
-        message: 'Enter your mongo collection name: ',
-        default: 'collection',
-        when: (answers => answers.setupMongoose ) // only ask if User wants to SETUP mongoose  
-      }, 
-      { // Mongoose URI , collection 
-        type: 'input',
-        name: 'uri - Mongoose connection string ',
-        default: (answers ) => 'mongodb://localhost:27017/'+answers. collectionName,
-        message: 'Change the URI if you want',
-        when: (answers => answers.setupMongoose ) // only ask if User wants to SETUP mongoose  
+        return "please enter a valid number";
       }
-    ]
-    
-    const serverPromt  = inquirer.createPromptModule();
-
-    let userInput = {} 
-
-    async function processInput(){
-      await createServer.createServer(userInput.PORT);
-      if(userInput.mongoose) await mongo.mongo();
+    },
+    {
+      // App Middleware
+      type: "checkbox",
+      name: "AppMiddleware",
+      mesasge: "Would you like to include the following App Middleware ?",
+      choices: [
+        { name: "Body Parser", value: "bodyParser", checked: true },
+        { name: "cors", value: "cors", checked: true }
+      ]
+      // , default: ['bodyParser','cors']
+    },
+    {
+      // mongoose ? yes/no
+      type: "list",
+      name: "pg",
+      message: "Would you like to include PostgreSQL?",
+      choices: [{ name: "Yes", value: true }, { name: "No", value: false }]
+    },
+    {
+      // mongoose ? yes/no
+      type: "list",
+      name: "mongoose",
+      message: "Would you like to Include MongoDB ?",
+      choices: [{ name: "Yes", value: true }, { name: "No", value: false }]
+    },
+    {
+      // further setup mongoose ?
+      type: "list",
+      name: "setupMongoose",
+      message: "Do you want to configure/setup MongooDB ?",
+      choices: [{ name: "Yes", value: true }, { name: "No", value: false }],
+      when: answers => answers.mongoose // only ask if User includes Mongoose
+    },
+    {
+      // Mongoose collection name
+      type: "input",
+      name: "collectionName",
+      message: "Enter your mongo collection name: ",
+      default: "collection",
+      when: answers => answers.setupMongoose // only ask if User wants to SETUP mongoose
+    },
+    {
+      // Mongoose URI , collection
+      type: "input",
+      name: "uri - Mongoose connection string ",
+      default: answers => "mongodb://localhost:27017/" + answers.collectionName,
+      message: "Change the URI if you want",
+      when: answers => answers.setupMongoose // only ask if User wants to SETUP mongoose
+    },
+    {
+      // React ? yes/no
+      type: "list",
+      name: "react",
+      message: "Would you like to include React?",
+      choices: [{ name: "Yes", value: true }, { name: "No", value: false }]
+    },
+    {
+      // Webpack ? yes/no
+      type: "list",
+      name: "webpack",
+      message: "Would you like to include Webpack?",
+      choices: [{ name: "Yes", value: true }, { name: "No", value: false }]
     }
+  ];
 
-    //promt user .... 
-    serverPromt(serverQuestions) 
-    .then((answers)=> userInput= answers) 
-    .then(()=>{
-      processInput();
-    })
-    .then(()=> console.log(userInput))
-    .catch(err => console.log('OhOh: ',err))
+  const serverPromt = inquirer.createPromptModule();
+
+  let userInput = {};
+
+  async function processInput() {
+    await createServer.createServer(userInput.PORT);
+    if (userInput.pg) await pg.pg();
+    if (userInput.mongoose) await mongo.mongo();
+    if (userInput.react) await react.react();
+    if (userInput.webpack) await webpack.webpack();
   }
 
- 
+  // promt user ....
+  serverPromt(serverQuestions)
+    .then(answers => (userInput = answers))
+    .then(() => {
+      processInput();
+    })
+    .then(() => console.log(userInput))
+    .catch(err => console.log("OhOh: ", err));
+};
--- bin/index.js
+++ bin/index.js
@@ -3,125 +3,140 @@
 pass the file to for execution */
 
 const fs = require("fs");
-var inquirer = require('inquirer');
-const clear = require('clear');
+var inquirer = require("inquirer");
+const clear = require("clear");
 
-const webpack = require ('./action_webpack'); 
-const createServer = require ('./action_server'); 
-const react = require('./action_react')
-const pg = require('./action_pg')
-const mongo = require('./action_mongo')
-const fullConfig = require('./fullConfig')
+const webpack = require("./action_webpack");
+const createServer = require("./action_server");
+const react = require("./action_react");
+const pg = require("./action_pg");
+const mongo = require("./action_mongo");
+const fullConfig = require("./fullConfig");
 
 const actions = {
   react: react.react,
   webpack: webpack.webpack,
   server: createServer,
   fullConfig: fullConfig
-}
+};
 
 /*Using slice below we ensure we are obtaining only the arguments after the first
 two elements in the process.argv array.*/
 let arguments = process.argv.slice(2);
 
-const action = arguments[0] ; 
+const action = arguments[0];
 
-if (  !(action in actions)  ){
-  return console.log( `"${action}" is not YET implemented. \n availble commands are : ${Object.keys(actions)}`)
+if (!(action in actions)) {
+  return console.log(
+    `"${action}" is not YET implemented. \n availble commands are : ${Object.keys(
+      actions
+    )}`
+  );
 }
 
-
-if(action == 'fullConfig'){
-  userInterface()
+if (action == "fullConfig") {
+  fullConfig.fullConfig();
 }
 
-function userInterface () {
+// function userInterface() {
+//   const setupServer = {
+//     PORT: 3000,
+//     bodyParser: false,
+//     cors: false,
+//     mongoose: {}
+//   };
 
-  const setupServer = {
-      PORT: 3000,  
-      bodyParser: false, 
-      cors: false, 
-      mongoose: {
-      }
-    }
+//   const serverQuestions = [
+//     {
+//       // PORT
+//       type: "input",
+//       name: "PORT",
+//       message: "PORT (default:3000) ",
+//       default: 3000,
+//       validate: value => {
+//         if (Number.isInteger(parseInt(value)) && value > 0) {
+//           return true;
+//         } else {
+//           return "please enter a valid number";
+//         }
+//       }
+//     },
+//     {
+//       // App Middleware
+//       type: "checkbox",
+//       name: "AppMiddleware",
+//       mesasge: "Would you like to include the following App Middleware?",
+//       choices: [
+//         { name: "Body Parser", value: "bodyParser", checked: true },
+//         { name: "cors", value: `cors`, checked: true }
+//       ]
+//       //, default: ['bodyParser','cors']
+//     },
+//     {
+//       // mongoose ? yes/no
+//       type: "list",
+//       name: "mongoose",
+//       message: "Would you like to include MongoDB?",
+//       choices: [{ name: "Yes", value: true }, { name: "No", value: false }]
+//     },
+//     {
+//       // further setup mongoose ?
+//       type: "list",
+//       name: "setupMongoose",
+//       message: "Do you want to configure/setup MongoDB?",
+//       choices: [{ name: "Yes", value: true }, { name: "No", value: false }],
+//       when: answers => answers.mongoose // only ask if User includes Mongoose
+//     },
+//     {
+//       // Mongoose collection name
+//       type: "input",
+//       name: "collectionName",
+//       message: "Enter your mongo collection name: ",
+//       default: "collection",
+//       when: answers => answers.setupMongoose // only ask if User wants to SETUP mongoose
+//     },
+//     {
+//       // Mongoose URI , collection
+//       type: "input",
+//       name: "uri - Mongoose connection string ",
+//       default: answers => "mongodb://localhost:27017/" + answers.collectionName,
+//       message: "Change the URI if you want",
+//       when: answers => answers.setupMongoose // only ask if User wants to SETUP mongoose
+//     },
+//     {
+//       // React ? yes/no
+//       type: "list",
+//       name: "React",
+//       message: "Would you like to include React?",
+//       choices: [{ name: "Yes", value: true }, { name: "No", value: false }]
+//     },
+//     {
+//       // Webpack ? yes/no
+//       type: "list",
+//       name: "Webpack",
+//       message: "Would you like to include Webpack?",
+//       choices: [{ name: "Yes", value: true }, { name: "No", value: false }]
+//     }
+//   ];
 
-    const serverQuestions = [
-      { // PORT 
-        type: 'input',
-        name: 'PORT',
-        message: 'PORT (default:3000) ',
-        default: 3000,
-        validate: (value)=>{
-          if( Number.isInteger(parseInt(value)) && value > 0 ){
-            return true;
-          } else {
-            return 'please enter a valid number';
-          }
-        }
-      },
-      { // App Middleware
-        type: 'checkbox',
-        name: 'AppMiddleware',
-        mesasge: 'Would you like to include the following App Middleware ?',
-        choices: [
-          {name: 'Body Parser', value:'bodyParser',checked: true},
-          {name: 'cors', value:`cors`,checked: true}
-        ]
-      //, default: ['bodyParser','cors']
-      },
-      { // mongoose ? yes/no 
-        type: 'list',
-        name: 'mongoose',
-        message: 'Would you like to Include Mongoose ?',
-        choices:[
-          {name: 'Yes', value:true},
-          {name: 'No', value:false}
-        ]
-      },
-      { // further setup mongoose ? 
-        type: 'list',
-        name: 'setupMongoose',
-        message: 'Do you want to configure/setup Mongoose ?',
-        choices:[
-          {name: 'Yes', value:true},
-          {name: 'No', value:false}
-        ],
-        when: (answers => answers.mongoose ) // only ask if User includes Mongoose 
-      },
-      { // Mongoose collection name
-        type: 'input',
-        name: 'collectionName',
-        message: 'Enter your mongo collection name: ',
-        default: 'collection',
-        when: (answers => answers.setupMongoose ) // only ask if User wants to SETUP mongoose  
-      }, 
-      { // Mongoose URI , collection 
-        type: 'input',
-        name: 'uri - Mongoose connection string ',
-        default: (answers ) => 'mongodb://localhost:27017/'+answers. collectionName,
-        message: 'Change the URI if you want',
-        when: (answers => answers.setupMongoose ) // only ask if User wants to SETUP mongoose  
-      }
-    ]
-    
-    const serverPromt  = inquirer.createPromptModule();
+//   const serverPromt = inquirer.createPromptModule();
 
-    let userInput = {} 
+//   let userInput = {};
 
-    async function processInput(){
-      await createServer.createServer(userInput.PORT);
-      if(userInput.mongoose) await mongo.mongo();
-    }
+//   async function processInput() {
+//     await createServer.createServer(userInput.PORT);
+//     if (userInput.mongoose) await mongo.mongo();
+//   }
 
-    //promt user .... 
-    serverPromt(serverQuestions) 
-    .then((answers)=> userInput= answers) 
-    .then(()=>{
-      processInput();
-    })
-    .then(()=> console.log(userInput))
-    .catch(err => console.log('OhOh: ',err))
-  }
+//   //promt user ....
+//   serverPromt(serverQuestions)
+//     .then(answers => (userInput = answers))
+//     .then(() => {
+//       processInput();
+//     })
+//     .then(() => console.log(userInput))
+//     .catch(err => console.log("OhOh: ", err));
+// }
 /* The "bin" object key in package.json, in our case "boil", is the CLI command
 associated with our CLI tool. Its value is the file to be executed, in our case
 ./bin/index.js */
@@ -131,8 +146,6 @@ associated with our CLI tool. Its value is the file to be executed, in our case
 /* The lib folder contains other files which index.js might use. These files
 should contain all CLI tool logic. We have not yet modularized our code to follow
 this practice. */
-
-
 
 // Could require in below file, and/or others, containing further logic
 // const logic = require("./lib/greet");
@@ -159,12 +172,11 @@ if (action == "server") {
   createServer.createServer();
 
   if (arguments[1] == "pg") {
-    pg.pg()
-  } 
+    pg.pg();
+  }
   if (arguments[1] == "mongo") {
     mongo.mongo();
   }
-
 }
 
 /* On command "boil webpack" boil creates a webpack configuration file within the root directory.*/
